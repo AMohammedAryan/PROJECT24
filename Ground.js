07794ad65add00eeffa177281600d78787f2e23c
AMohammedAryan <69350797+AMohammedAryan@users.noreply.github.com>
2020-08-30 12:53:35 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -3,7 +3,8 @@ class Ground{
     constructor(){
 
         var options={
-            density:100.0
+            density:100.0,
+            isStatic:true
         }
 
         this.body = Bodies.rectangle(width/2, height-10, width, 50, options);
